109eb52c91a0829a576c30879b0acb6408848e6a
Nimesh Ghimire <nimesh.ghimire99@gmail.com>
2026-02-25 16:58:16 +0100

    added to check in admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User
+@admin.register(User)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'role']
 
-# Register your models here.
