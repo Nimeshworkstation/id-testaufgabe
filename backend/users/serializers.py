a055307d2171334b24b5a3ad1bb500ab3d1891e2
Nimesh Ghimire <nimesh.ghimire99@gmail.com>
2026-02-25 19:07:34 +0100

    validation for username and email

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 from .models import User
 
+
+
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'role']
-        extra_kwargs = {
-            'password': {'write_only': True}  
-        }
+        fields = ["id", "username", "password", "first_name", "last_name", "email", "role"]
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("This username is already taken.")
+        return value
+    
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already taken.")
+        return value
