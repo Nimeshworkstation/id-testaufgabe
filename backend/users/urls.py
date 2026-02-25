c362972bfd20dd03cf518d9b75332b850bfd7c6d
Nimesh Ghimire <nimesh.ghimire99@gmail.com>
2026-02-25 16:50:51 +0100

    added views and urls for user creation, login, logout and profile

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView,LoginView,ProfileView
 
 urlpatterns = [
       path('register/', RegisterView.as_view(), name='register'),
-  
+        path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', ProfileView.as_view(), name='user'),
 ]
+  
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
-from .serializers import UserSerializer
-from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from django.contrib.auth import authenticate, login
+from .serializers import UserSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework.authtoken.models import Token
 
 
-# Create your views here.
 class RegisterView(APIView):
     permission_classes = [AllowAny]
     def post(self, request):
@@ -16,3 +17,38 @@ class RegisterView(APIView):
             return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'user': UserSerializer(user).data
+            })
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response({'message': 'Logged out successfully'})
+
+class ProfileView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response(UserSerializer(request.user).data)
+    
