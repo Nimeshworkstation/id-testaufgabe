857a25496863855a26e42b645fc209841c79e963
Nimesh Ghimire <nimesh.ghimire99@gmail.com>
2026-02-25 20:45:44 +0100

    handled navbar for login and signup

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router";
+import useAuthStore from "../store/authStore";
 
 export default function Navbar() {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
   return (
     <nav className="navbar">
       <Link to="/">ID App</Link>
       <div>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
+        {!isAuthenticated && <Link to="/login">Login</Link>}
+        {!isAuthenticated && <Link to="/signup">Signup</Link>}
       </div>
     </nav>
   );
