d9343c7cd7f2b5e5dfa002aa6c32acb516e05191
Nimesh Ghimire <nimesh.ghimire99@gmail.com>
2026-02-25 15:46:58 +0100

    creating url for user register, and serialize it, creating views too

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+
 
 # Create your views here.
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
