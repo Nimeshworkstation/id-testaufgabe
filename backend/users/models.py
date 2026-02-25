b5ccd3252336b28c066a7236e6570c93083f21be
Nimesh Ghimire <nimesh.ghimire99@gmail.com>
2026-02-25 14:12:13 +0100

    created a model users using abstract built in model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class RoleChoice(models.TextChoices):
+    CUSTOMER = 'customer', 'Customer'
+    TEAM = 'team', 'Creative Team'
+    MANAGEMENT = 'management', 'Management'
+
+
+class User(AbstractUser):
+    role = models.CharField(max_length=20, choices=RoleChoice.choices, default=RoleChoice.CUSTOMER)
