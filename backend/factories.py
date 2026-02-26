210dce7b15e242f50e0928a845f7cdf09ad29313
Nimesh <nimesh.ghimire99@hotmail.com>
2026-02-26 09:21:33 +0100

    added all fields

--- backend/factories.py
+++ backend/factories.py
@@ -5,6 +5,11 @@ from users.models import User, RoleChoice
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
+    username = factory.Sequence(lambda n: f"user{n}")
+    email = factory.Sequence(lambda n: f"user{n}@example.com")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    password = factory.PostGenerationMethodCall("set_password", "testpass123")
 
 
 
