8db24f86d9e487c0ec015f51c1dab98cab829a8e
Nimesh <nimesh.ghimire99@hotmail.com>
2026-02-26 09:41:07 +0100

    added test for Userserializer

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from users.models import RoleChoice
 from .factories import UserFactory, TeamUserFactory, ManagementUserFactory, CustomerUserFactory
-
+from .serializers import UserSerializer
 class UserModelTest(TestCase):
 
     def test_customer_role(self):
@@ -29,3 +29,36 @@ class UserModelTest(TestCase):
         user1 = UserFactory()
         user2 = UserFactory()
         self.assertNotEqual(user1.email, user2.email)
+
+
+class UserSerializerTest(TestCase):
+
+
+    def test_rejects_duplicate_username(self):
+        existing = CustomerUserFactory()
+        data = {
+            "username": existing.username,
+            "email": CustomerUserFactory.build().email,
+            "first_name": existing.first_name,
+            "last_name": existing.last_name,
+            "password": "testpass123",
+            "role": RoleChoice.CUSTOMER,
+        }
+        serializer = UserSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn("username", serializer.errors)
+
+    def test_rejects_duplicate_email(self):
+        existing = CustomerUserFactory()
+        data = {
+            "username": CustomerUserFactory.build().username,
+            "email": existing.email,
+            "first_name": existing.first_name,
+            "last_name": existing.last_name,
+            "password": "testpass123",
+            "role": RoleChoice.CUSTOMER,
+        }
+        serializer = UserSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn("email", serializer.errors)
+
