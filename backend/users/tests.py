2a097db8c87162600f93201e9d94f1a9cf1f3bc0
Nimesh <nimesh.ghimire99@hotmail.com>
2026-02-26 09:31:02 +0100

    added model test

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from users.models import RoleChoice
+from .factories import UserFactory, TeamUserFactory, ManagementUserFactory, CustomerUserFactory
 
-# Create your tests here.
+class UserModelTest(TestCase):
+
+    def test_customer_role(self):
+        user = CustomerUserFactory()
+        self.assertEqual(user.role, RoleChoice.CUSTOMER)
+
+    def test_team_role(self):
+        user = TeamUserFactory()
+        self.assertEqual(user.role, RoleChoice.TEAM)
+
+    def test_management_role(self):
+        user = ManagementUserFactory()
+        self.assertEqual(user.role, RoleChoice.MANAGEMENT)
+
+    def test_password_is_hashed(self):
+        user = UserFactory()
+        self.assertTrue(user.check_password("testpass123"))
+
+    def test_unique_username(self):
+        user1 = UserFactory()
+        user2 = UserFactory()
+        self.assertNotEqual(user1.username, user2.username)
+
+    def test_unique_email(self):
+        user1 = UserFactory()
+        user2 = UserFactory()
+        self.assertNotEqual(user1.email, user2.email)
