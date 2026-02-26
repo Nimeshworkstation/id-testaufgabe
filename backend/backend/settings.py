b0b43bb62f31cc6cafc1a5fd0313e4a477832c0c
Nimesh <nimesh.ghimire99@hotmail.com>
2026-02-26 13:12:02 +0100

    changed settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,6 +19,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+DEBUG = os.environ.get('DEBUG', '1')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
