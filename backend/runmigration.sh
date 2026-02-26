d0487b6e98d951c22101f29b1991b44ff9d613d5
Nimesh <nimesh.ghimire99@hotmail.com>
2026-02-26 10:49:40 +0100

    added docker file for frontend and backend, docker compose

---
+++ backend/runmigration.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Applying database migrations..."
+python manage.py migrate
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+echo "Starting server..."
+exec "$@"
